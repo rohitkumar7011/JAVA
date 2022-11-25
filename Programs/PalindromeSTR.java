b6e57949a144a8eac4a6653f6267547cc9e4d091
rohitkumar7011 <97799802+rohitkumar7011@users.noreply.github.com>
2022-11-25 19:54:11 +0530

    Add files via upload

---
+++ Programs/PalindromeSTR.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class PalindromeSTR {
+    public static void main(String[] args) {
+        System.out.println("enter the string you want to reverse");
+        Scanner sc= new Scanner(System.in);
+        String str= sc.nextLine(); 
+        String check = str;
+        String rev = "";
+        int len = str.length()-1;
+        while(len >= 0){
+            rev = rev + (str.charAt(len));
+            len = len-1;
+           
+
+        }
+        if( rev ==  check){
+            System.out.println("The string is Palindrome");
+        }
+        else{
+            System.out.println("Not a Palindrome Str.");
+        }
+    }
+    
+}
