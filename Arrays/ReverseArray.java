7380c1ad3dde61b3fb6e93c22adc9181d9ec8a76
Rohit Kumar <rohit.verrma13@gmail.com>
2023-02-26 22:42:28 +0530

    Add files via upload

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array:");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+}
