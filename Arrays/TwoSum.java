13497e7ffca66b33927870a2efbae0a8ba0007c8
Rohit Kumar <rohit.verrma13@gmail.com>
2023-02-27 22:20:48 +0530

    Add files via upload

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,7 +1,17 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class TwoSum {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array:");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
     }
 }
