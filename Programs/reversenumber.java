4a4a3c4bd51b6dd727194b9e37f2138aa90292ee
Rohit Kumar <rohit.verrma13@gmail.com>
2023-03-03 22:36:32 +0530

    Add files via upload

--- Programs/reversenumber.java
+++ Programs/reversenumber.java
@@ -1,6 +1,15 @@
-package Programs;
+import java.util.Scanner;
+
 public class reversenumber {
     public static void main(String[] args) {
-        
-    }   
+        System.out.println("enter the number:");
+        int n = new Scanner(System.in).nextInt();
+        int rev = 0;
+        while (n > 0) {
+            rev = rev * 10 + (n % 10);
+            n = n / 10;
+        }
+        System.out.println(rev);
+
+    }
 }
