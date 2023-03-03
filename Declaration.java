f223fd73704fdbae3ba4f1fa7497d21da7564933
Rohit Kumar <rohit.verrma13@gmail.com>
2023-03-03 23:17:14 +0530

    Create Declaration.java

---
+++ Declaration.java
@@ -0,0 +1,9 @@
+public class DeclarationType{
+    public static void main(String[] args) {
+        int arr_1[] = {1,3,4,5,5};
+        int []arr_2 = {1,2,3,4,5};
+        int[] arr_3 = {1,2,3,4,5};
+        int[] arr_4 = new int[4];
+        int arr[] = new int[]{1,2,3,4};
+    }
+}
