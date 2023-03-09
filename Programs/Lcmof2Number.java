f4fb8d506df589be3c3d53680f137a0b8cdf31a1
Rohit Kumar <rohit.verrma13@gmail.com>
2023-03-09 22:39:37 +0530

    Add files via upload

---
+++ Programs/Lcmof2Number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Lcmof2Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int max = 0;
+        if (a > b)
+            max = a;
+        else
+            max = b;
+        while (true) {
+            if (max % a == 0 && max % b == 0)
+                break;
+            max++;
+        }
+        System.out.println(max);
+    }
+}
