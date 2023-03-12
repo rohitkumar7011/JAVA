3077a91d953bc347818ba5e1fb3457d7d4f33fba
Rohit Kumar <rohit.verrma13@gmail.com>
2023-03-12 23:59:59 +0530

    Update HelloWorld.java

--- Programs/HelloWorld.java
+++ Programs/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("my first program");
-        System.out.println("hello world");
+        System.out.println("my first program :");
+        System.out.println("hello world...");
     }
 }
